c8bd81a3a81f5c6e3a52d5831738986ba3260989
sam <boadusamuel@gmail.com>
2022-12-04 00:19:56 +0000

    linked get all notes endpoint to backend

--- client/src/components/Note.js
+++ client/src/components/Note.js
@@ -1,35 +1,51 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import NoteCard from "./NoteCard";
 
+export default function Note() {
+  const [notes, setNote] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [previousPage, setPreviousPage] = useState(0);
+  const [isLastList, setIsLastList] = useState(false);
+  const listInnerRef = useRef();
 
-export default function Note(){
-    const [notes, setNote] = useState([]);
-
-
+  useEffect(() => {
     const fetchData = async () => {
-        const response = await fetch('/api/notes');
-        if(!response.ok){
-            throw new Error('Something went wrong');
-        }else{       
-            return response.json()
+      const response = await axios.get(
+        `/api/v1.0/notes?page=${currentPage}&size=10`
+      );
+
+      if (!response.data.success) {
+        throw new Error("Something went wrong");
+      } else {
+        let data = response.data.data;
+        if (!data.notes.length) {
+          setIsLastList(true);
+          return;
         }
+        setPreviousPage(currentPage);
+        setNote([...notes, ...data.notes]);
+      }
+    };
+    if (!isLastList && previousPage !== currentPage) {
+      fetchData();
     }
+  }, [currentPage, previousPage, notes, isLastList]);
 
-    useEffect(() => fetchData().then((data) => setNote(data.notes)).catch((e) => console.log(e)), [])
+  const onScroll = () => {
+    if (listInnerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
+      if (scrollTop + clientHeight === scrollHeight) {
+        setCurrentPage(currentPage + 1);
+      }
+    }
+  };
 
-    return  (
-       <div>
-       {
-        notes.map((note, index) => 
-        {
-            return (
-                <div key={index}>
-                     <p>{note.title}</p>
-                     <p>{note.body}</p>
-                </div>
-            )
-        }
-        )
-       }
-       </div>
-    );
+  return (
+    <NoteCard
+    onScroll={onScroll}
+    listInnerRef={listInnerRef}
+    notes={notes}
+  />
+  );
 }
